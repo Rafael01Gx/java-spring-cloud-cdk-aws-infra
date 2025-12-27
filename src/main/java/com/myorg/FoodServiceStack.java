c47a3aadb07a369e2a4f59b1b73a317997edd418
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-12-27 17:52:15 -0300

    Configura Log Group

--- src/main/java/com/myorg/FoodServiceStack.java
+++ src/main/java/com/myorg/FoodServiceStack.java
@@ -1,15 +1,22 @@
 package com.myorg;
 
 import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecr.IRepository;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
 import java.util.Map;
+
+import static software.amazon.awscdk.services.ecr.Repository.fromRepositoryName;
 
 public class FoodServiceStack extends Stack {
 
@@ -25,6 +32,8 @@ public class FoodServiceStack extends Stack {
                 "SPRING_DATASOURCE_USERNAME", "admin",
                 "SPRING_DATASOURCE_PASSWORD", Fn.importValue("pedidos-db-senha"));
 
+        IRepository iRepositorio = fromRepositoryName(this,"repositorio","img-pedidos-ms");
+
         ApplicationLoadBalancedFargateService.Builder.create(this,"FoodService")
                 .serviceName("food-service")
                 .cluster(cluster)
@@ -33,10 +42,23 @@ public class FoodServiceStack extends Stack {
                 .listenerPort(8080)
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
-                                .image(ContainerImage.fromRegistry("amazon/amazon-ecs-sample"))
+                                .image(//ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
+                                        ContainerImage.fromEcrRepository(iRepositorio, "latest")
+
+                                )
                                 .containerPort(8080)
                                 .containerName("food-app")
                                 .environment(env)
+                                .logDriver(LogDriver.awsLogs(
+                                        AwsLogDriverProps.builder()
+                                                .logGroup(LogGroup.Builder
+                                                        .create(this,"PedidosMsLogGroup")
+                                                        .logGroupName("PedidosMsLog")
+                                                        .removalPolicy(RemovalPolicy.DESTROY)
+                                                        .build())
+                                                .streamPrefix("PedidosMs")
+                                                .build()
+                                ))
                                 .build()
                 )
                 .memoryLimitMiB(1024)
